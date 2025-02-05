b6467c681e98ad8d2edc6e38cc8c86dd401117f3
karcher <karchernic@gmail.com>
2025-02-05 14:11:41 +0100

    un-commenting code lines I forgot to uncomment

--- scripts/PRISMA/2024_06_14_prepare_PRISMA.r
+++ scripts/PRISMA/2024_06_14_prepare_PRISMA.r
@@ -328,9 +328,9 @@ outcomeInformation <- outcomeInformation %>%
         # cyp3a4star22 = v64_CYP3A4_22,
     ) %>%
     rename(all_of(model_covariates)) %>%
-    #mutate(birthday = as.Date(birthday)) %>%
-    mutate(age = age_calc(birthday, as.Date("2023-11-07"), "years")) %>%
-    mutate(ageCategorical = ifelse(age > 18, "adult", 'non-adult'))
+    mutate(birthday = as.Date(birthday)) %>%
+    #mutate(age = age_calc(birthday, as.Date("2023-11-07"), "years")) %>%
+    #mutate(ageCategorical = ifelse(age > 18, "adult", 'non-adult'))
 
 stopifnot(all(outcomeInformation$patientID %in% profiles$PSN))
 ## [1]v4_cyp_genotype =  ";\"\";1;\"CYP3A5(*3) Positive\";2;\"CYP3A5(*3) Negative\""
